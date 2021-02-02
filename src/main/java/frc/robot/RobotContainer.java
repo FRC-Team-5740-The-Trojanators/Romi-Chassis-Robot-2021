0a6d97642b71ae90ea11a2e0d92df32ef3a23999
Seth Peasley <seth.peasley@gmail.com>
2021-02-02 15:44:15 -0500

    Added ability to load a pre-made trajectory file.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,12 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
@@ -28,6 +32,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -52,6 +57,8 @@ public class RobotContainer
     // Create SmartDashboard chooser for autonomous routines
     private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
+    private Trajectory m_trajectory; 
+
     // NOTE: The I/O pin functionality of the 5 exposed I/O pins depends on the hardware "overlay"
     // that is specified when launching the wpilib-ws server on the Romi raspberry pi.
     // By default, the following are available (listed in order from inside of the board to outside):
@@ -66,9 +73,32 @@ public class RobotContainer
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() 
     {
+        //load Trajectory
+        loadTrajectory();
+
+
         // Configure the button bindings
         configureButtonBindings();
     }
+
+    public void loadTrajectory()
+    {
+        String trajectoryJSON = "paths/PathTest1.wpilib.json";
+        m_trajectory = new Trajectory();
+
+        try
+        {
+            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+            m_trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        }
+        catch (IOException ex)
+        {
+            DriverStation.reportError("Unable to open trajectory File: " + trajectoryJSON, ex.getStackTrace());
+        }
+    }
+
+
+
 
     /**
      * Use this method to define your button->command mappings. Buttons can be created by
@@ -117,82 +147,70 @@ public class RobotContainer
     }
 
 
-/**
-   * Generate a trajectory following Ramsete command
-   * 
-   * This is very similar to the WPILib RamseteCommand example. It uses
-   * constants defined in the Constants.java file. These constants were 
-   * found empirically by using the frc-characterization tool.
-   * 
-   * @return A SequentialCommand that sets up and executes a trajectory following Ramsete command
+    /**
+     * Generate a trajectory following Ramsete command
+     * 
+     * This is very similar to the WPILib RamseteCommand example. It uses
+     * constants defined in the Constants.java file. These constants were 
+     * found empirically by using the frc-characterization tool.
+     * 
+     * @return A SequentialCommand that sets up and executes a trajectory following Ramsete command
    */
-  private Command generateRamseteCommand() {
-    var autoVoltageConstraint =
-        new DifferentialDriveVoltageConstraint(
-            new SimpleMotorFeedforward(DriveConstants.ksVolts, 
-                                       DriveConstants.kvVoltSecondsPerMeter, 
-                                       DriveConstants.kaVoltSecondsSquaredPerMeter),
-            DriveConstants.kDriveKinematics,
-            10);
+    private Command generateRamseteCommand() 
+    {
+        var autoVoltageConstraint =
+            new DifferentialDriveVoltageConstraint(
+                new SimpleMotorFeedforward(DriveConstants.ksVolts, 
+                                           DriveConstants.kvVoltSecondsPerMeter, 
+                                           DriveConstants.kaVoltSecondsSquaredPerMeter),
+                DriveConstants.kDriveKinematics,
+                10);
 
-    TrajectoryConfig config =
-        new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, 
-                             AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            .setKinematics(DriveConstants.kDriveKinematics)
-            .addConstraint(autoVoltageConstraint);
+        TrajectoryConfig config =
+            new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, 
+                                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                .setKinematics(DriveConstants.kDriveKinematics)
+                .addConstraint(autoVoltageConstraint);
 
-    // This trajectory can be modified to suit your purposes
-    // Note that all coordinates are in meters, and follow NWU conventions.
-    // If you would like to specify coordinates in inches (which might be easier
-    // to deal with for the Romi), you can use the Units.inchesToMeters() method
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction
-        new Pose2d(0, 0, new Rotation2d(0)),
-        List.of(
-            new Translation2d(0.5, 0.25),
-            new Translation2d(1.0, -0.25),
-            new Translation2d(1.5, 0)
-        ),
-        new Pose2d(0.0, 0, new Rotation2d(Math.PI)),
-        config);
+        // This trajectory can be modified to suit your purposes
+        // Note that all coordinates are in meters, and follow NWU conventions.
+        // If you would like to specify coordinates in inches (which might be easier
+        // to deal with for the Romi), you can use the Units.inchesToMeters() method
+        Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            List.of(
+                new Translation2d(0.5, 0.25),
+                new Translation2d(1.0, -0.25),
+                new Translation2d(1.5, 0)
+            ),
+            new Pose2d(0.0, 0, new Rotation2d(Math.PI)),
+            config);
 
-    // RamseteCommand ramseteCommand = new RamseteCommand(
-    //     exampleTrajectory,
-    //     m_drivetrain::getPose,
-    //     new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-    //     new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter),
-    //     DriveConstants.kDriveKinematics,
-    //     m_drivetrain::getWheelSpeeds,
-    //     new PIDController(DriveConstants.kPDriveVel, 0, 0),
-    //     new PIDController(DriveConstants.kPDriveVel, 0, 0),
-    //     m_drivetrain::tankDriveVolts,
-    //     m_drivetrain);
-
-    RamseteCommand ramseteCommand = new RamseteCommand(
-        exampleTrajectory, 
-        m_drivetrain::getPose, 
-        new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-        new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter), 
-        DriveConstants.kDriveKinematics, 
-        m_drivetrain::getWheelSpeeds,
-        new PIDController(DriveConstants.kPDriveVel, 0, 0), 
-        new PIDController(DriveConstants.kPDriveVel, 0, 0),
-        m_drivetrain::tankDriveVolts, 
-        m_drivetrain);
+        RamseteCommand ramseteCommand = new RamseteCommand(
+            m_trajectory, 
+            m_drivetrain::getPose, 
+            new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+            new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter), 
+            DriveConstants.kDriveKinematics, 
+            m_drivetrain::getWheelSpeeds,
+            new PIDController(DriveConstants.kPDriveVel, 0, 0), 
+            new PIDController(DriveConstants.kPDriveVel, 0, 0),
+            m_drivetrain::tankDriveVolts, 
+            m_drivetrain);
 
 
+        m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
 
-    m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose());
+        // Set up a sequence of commands
+        // First, we want to reset the drivetrain odometry
+        return new InstantCommand(() -> m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose()), m_drivetrain)
+            // next, we run the actual ramsete command
+            .andThen(ramseteCommand)
 
-    // Set up a sequence of commands
-    // First, we want to reset the drivetrain odometry
-    return new InstantCommand(() -> m_drivetrain.resetOdometry(exampleTrajectory.getInitialPose()), m_drivetrain)
-        // next, we run the actual ramsete command
-        .andThen(ramseteCommand)
-
-        // Finally, we make sure that the robot stops
-        .andThen(new InstantCommand(() -> m_drivetrain.tankDriveVolts(0, 0), m_drivetrain));
-  } 
+            // Finally, we make sure that the robot stops
+            .andThen(new InstantCommand(() -> m_drivetrain.tankDriveVolts(0, 0), m_drivetrain));
+    } 
 
 
 
